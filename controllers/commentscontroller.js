d0af6c093c01da8137e3ccdba05dc5a71e417079
Jaylen Wilson <Jaylenaverywilson@gmail.com>
2022-01-27 11:30:51 -0500

    update and delete endpoints created and tested for comment controller no issues moving on to client side for now will come back to server to add admin logic and any extra routes I decide to implement

--- controllers/commentscontroller.js
+++ controllers/commentscontroller.js
@@ -30,4 +30,59 @@ router.post('/comment', validateJWT, async (req, res) => {
     };
 });
 
+// DELETE #2 DELETE A SPECIFIC COMMENT
+router.delete('/delete/:userId/:postId/:id', validateJWT, async (req, res) => {
+    const commentId = req.params.id
+    const userId = req.params.userId
+    const postId = req.params.postId
+
+    try {
+        const result = await models.CommentsModel.destroy({
+            where: {
+                id: commentId,
+                userId: userId,
+                postId: postId
+            },
+        });
+        res.status(200).json({
+            message: `comment has been deleted: ${result} `,
+
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: `could not delete post ${err}`
+        })
+    }
+})
+
+
+
+// UPDATE #2 UPDATE A SPECIFIC COMMENT
+router.put('/edit/:userId/:postId/:id', validateJWT, async (req, res) => {
+    const { content } = req.body.comments;
+    const commentId = req.params.id;
+    const userId = req.params.userId;
+    const postId = req.params.postId
+    try {
+
+        const updatedComment = await models.CommentsModel.update({
+            content: content
+        },
+            {
+                where: {
+                    id: commentId,
+                    postId: postId,
+                    userId: userId
+                }
+            }
+        )
+        res.status(200).json({
+            message: "post updated",
+            updatedComment: updatedComment
+        });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    };
+});
+
 module.exports = router
